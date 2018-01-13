d39bab2acfe013ffceff6c7fd7221a86be927f29
DESKTOP-QIBCDI7\Michal <mi.kaluzinski@gmail.com>
2018-01-13 15:28:50 +0100

    - dodanie nowych permissions - dodanie edycji nazwy/password dla grupy

--- shopping_list_api/groups/api/serializers.py
+++ shopping_list_api/groups/api/serializers.py
@@ -17,6 +17,8 @@ class ShoppingGroupCreateSerializer(serializers.HyperlinkedModelSerializer):
         shopping_group = ShoppingGroup(owner=user, name=validated_data["name"])
         shopping_group.password = validated_data['password']
         shopping_group.save()
+        shopping_group.members.add(user)
+        shopping_group.save()
         return shopping_group
 
     def validate_name(self, value):
@@ -27,4 +29,28 @@ class ShoppingGroupCreateSerializer(serializers.HyperlinkedModelSerializer):
             return value
 
 
+class ShoppingGroupUpdateDetailSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=100, required=False)
+    password = serializers.CharField(max_length=10, allow_null=False, allow_blank=False, write_only=True, required=False)
+    owner = UserDetailSerializer(read_only=True)
 
+    class Meta:
+        model = ShoppingGroup
+        fields = ('name', 'password', 'owner')
+
+    def update(self, instance, validated_data):
+        shopping_group = instance
+        print('xD')
+        if "name" in validated_data:
+            shopping_group.name = validated_data["name"]
+        if "password" in validated_data:
+            shopping_group.password = validated_data["password"]
+        shopping_group.save()
+        return shopping_group
+
+    def validate_name(self, value):
+        qs = ShoppingGroup.objects.filter(name__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError("This name has used.")
+        else:
+            return value
--- shopping_list_api/groups/api/urls.py
+++ shopping_list_api/groups/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import GroupCreateAPIView
+from .views import GroupCreateAPIView, GroupDetailUpdateAPIView
 urlpatterns = [
     url(r'^$', GroupCreateAPIView.as_view(), name='create'),
+    url(r'^(?P<pk>\d+)/$', GroupDetailUpdateAPIView.as_view(), name="detail-update"),
 ]
